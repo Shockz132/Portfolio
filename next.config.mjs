edda494f06ccbb27092d63775bc305e9c85775f7
Shockz132 <jethrochia1@gmail.com>
2025-01-07 03:47:17 +0800

    undo previous commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const repo = "Portfolio";
-const assetPrefix = `/${repo}/`;
-const basePath =`/${repo}`
-
-
 const nextConfig = {
-    trailingSlash: true,
-
     output: 'export',
-    basePath: basePath,
-    assetPrefix: assetPrefix,
-    images: {
-        unoptimized: true,
-    },
 };
 
 export default nextConfig;
